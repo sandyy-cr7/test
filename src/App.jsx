c64ddcc9c227c4fd23546777fecc0d301bd43fe6
Sandeep.s <Sandeep.s@YOEKI-PC-004>
2025-01-28 10:51:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+import { deepOrange, deepPurple } from '@mui/material/colors';
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello</h1> 
+      <Button variant="contained">Submit</Button> 
+      <Avatar sx={{ bgcolor: deepPurple[500] }}>Sa</Avatar>
+    </>
+  )
+}
+
+export default App
